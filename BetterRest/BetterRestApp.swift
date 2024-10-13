1eef29f8284c38ef2cc2e0118ffceae6530f6e0e
Meet Mistry <meet.m@simformsolutions.com>
2024-10-13 10:52:29 +0530

    Initial commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Meet Mistry on 13/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
